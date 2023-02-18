055d265a3843e32f7852138f584366f87c459e36
piousgungnir <piousgungnir@gmail.com>
2023-02-18 16:36:45 +0700

    started describing the implementation of methods for the CoordinatePoint table

--- src/database.hpp
+++ src/database.hpp
@@ -5,9 +5,11 @@
 #ifndef _DATABASE_HPP_
 #define _DATABASE_HPP_
 
+#include "domain/CoordinatePoint.h"
+#include "pqxx/pqxx"
+#include <iostream>
 #include <memory>
 #include <string>
-#include "pqxx/pqxx"
 
 namespace DB
 {
@@ -16,31 +18,118 @@ namespace DB
 class ICoordinatePointRepository
 {
 protected:
-    ICoordinatePointRepository() = default; // стандартный конструктор ?нужен ли, пусть будет
     virtual ~ICoordinatePointRepository() = default; // защищенный, чисто виртуальный деструктор
 
 public:
-    ICoordinatePointRepository(const ICoordinatePointRepository &) = delete;             // запрет копирования
-    ICoordinatePointRepository & operator=(const ICoordinatePointRepository &) = delete; // запрет присваивания
-
 //    инициализация базы данных ( создание таблиц )
     virtual void initDB() = 0;
 //    добавление в базу координатой точки
-    virtual void addCoordinatePoint() = 0;
+    virtual void addCoordinatePoint(impl::CoordinatePoint cp) = 0;
 //    получение координатной точки из базы по её id
-    virtual void getByIdCoordinatePoint() = 0;
+    virtual impl::CoordinatePoint getByIdCoordinatePoint(const std::string& idCP) = 0;
 };
 
-//реализация репозитория с использованием Postgresql
+//реализация репозитория с исползованием Postgresql
 //пока что 1 репозиторий - 1 подключение, но можно реализовать и с пулом подключений при асинхронной работе
 class PostgresCPRepository : ICoordinatePointRepository {
-
+private:
     std::unique_ptr<pqxx::connection> connection;
 public:
-    explicit PostgresCPRepository(const std::string& dbUrl) {
-        connection = std::make_unique<pqxx::connection>(dbUrl); //создаем новое подключение
+    void initDB() override {
+        pqxx::work txn(*connection);
+        txn.exec("create table \"coordinatepoint\" "
+                 "("
+                 "    \"idcoordinatepoint\"    BIGINT           not null"
+                 "        constraint \"CoordinatePoint_pk\""
+                 "            primary key, "
+                 "    \"timeCoordinatePoint\"  text       not null,"
+                 "    \"range \"               double precision not null,"
+                 "    \"azimuth \"             double precision not null,"
+                 "    \"amplitude \"           numeric          not null,"
+                 "    mode                   integer          not null,"
+                 "    \"missionCode\"          integer,"
+                 "    \"unitCode \"            integer,"
+                 "    \"bortNumber\"           integer,"
+                 "    altitude               integer,"
+                 "    spi                    bool,"
+                 "    \"xImpulse\"             bool,"
+                 "    \"militaryAlarm\"        bool,"
+                 "    \"isRollCall\"           bool,"
+                 "    \"isControlResponder\"   bool,"
+                 "    \"missionCodeIsGarbled\" bool,"
+                 "    \"unitCodeIsGarbled\"    bool,"
+                 "    \"bortNumberIsGarbled\"  bool,"
+                 "    \"heightIsGarbled\"      bool);");
+        txn.commit();
     }
 
+    PostgresCPRepository(const std::string& dbUrl, const bool& initDatabaseFlag) {
+        connection = std::make_unique<pqxx::connection>(dbUrl); //создаем новое подключение
+        if (initDatabaseFlag == true) {
+            initDB();
+        }
+        connection->prepare("addCP",
+                                   "INSERT INTO coordinatepoint "
+//                                   "(idcoordinatePoint, timecoordinatecoint, range, azimuth, amplitude, " //затуп с заглавными буквами, pqxx делает все lowcase, не понял
+//                                   "mode, missionCode, unitCode, bortNumber, altitude, "
+//                                   "spi, xImpulse, militaryAlarm, isRollCall, isControlResponder, "
+//                                   "missionCodeIsGarbled, unitCodeIsGarbled, bortNumberIsGarbled, heightIsGarbled) "
+                                   "VALUES ($1, $2, $3, $4, $5, "
+                                   "$6, $7, $7, $8, $9, "
+                                   "$10, $11, $12, $13, $14, "
+                                   "$15, $16, $17, $18)");
+
+        connection->prepare("getById",
+                            "SELECT * FROM coordinatepoint "
+                            "WHERE idcoordinatepoint = $1");
+    }
+
+    void addCoordinatePoint(impl::CoordinatePoint cp) override {
+        // Start a transaction
+        pqxx::work txn(*connection);
+        // query
+//        txn.exec_prepared("addCP",
+//                          2);
+        txn.exec_prepared("addCP",
+                          5, timestamp_to_str_datetime(cp.timestamp).c_str() ,1,1,1,
+                          1,1,1,1,1,
+                          false,false,false,false,false,
+                          false,false,false);
+        txn.commit();
+    }
+
+    impl::CoordinatePoint getByIdCoordinatePoint(const std::string& idCP) override {
+        pqxx::work txn(*connection);
+        pqxx::result response = txn.exec_prepared("getById",idCP);
+
+        impl::CoordinatePoint cp;
+
+//        for(auto column : response[0]){ // вывод на экран всех столбцов строки
+//            std::cout << pqxx::to_string(column) << std::endl;
+//        }
+
+        cp.idCoordinatePoint = std::stoi(pqxx::to_string(response[0][0]));
+//        cp.timestamp = pqxx::to_string(response[0][1]); // ??? пробелма с кастом, нагуглил только boost либы. И каст с postgrsql timestomp в time_point, тоже
+        cp.range = std::stod(pqxx::to_string(response[0][2]));
+        cp.azimuth = std::stod(pqxx::to_string(response[0][3]));
+        cp.amplitude = (uint16_t)std::stoll(pqxx::to_string(response[0][4])); // ??? как кастить си стринг в uint_16(
+        cp.mode = static_cast<SystemMode>(std::stoi(pqxx::to_string(response[0][5])));
+        cp.missionCode = std::stoi(pqxx::to_string(response[0][6]));
+        cp.unitCode = std::stoi(pqxx::to_string(response[0][7]));
+        cp.bortNumber = std::stoi(pqxx::to_string(response[0][8]));
+        cp.altitude = std::stoi(pqxx::to_string(response[0][9]));
+        std::istringstream(pqxx::to_string(response[0][10])) >> cp.spi;
+        std::istringstream(pqxx::to_string(response[0][11])) >> cp.xImpulse;
+        std::istringstream(pqxx::to_string(response[0][12])) >> cp.militaryAlarm;
+        std::istringstream(pqxx::to_string(response[0][13])) >> cp.isRollCall;
+        std::istringstream(pqxx::to_string(response[0][14])) >> cp.isControlResponder;
+        std::istringstream(pqxx::to_string(response[0][15])) >> cp.missionCodeIsGarbled;
+        std::istringstream(pqxx::to_string(response[0][16])) >> cp.unitCodeIsGarbled;
+        std::istringstream(pqxx::to_string(response[0][17])) >> cp.bortNumberIsGarbled;
+        std::istringstream(pqxx::to_string(response[0][18])) >> cp.heightIsGarbled;
+
+        return cp;
+    }
 };
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,152 @@
 #include "domain/CoordinatePoint.h"
-#include "domain/POIProtocol.h"
+#include "database.hpp"
 
 int main()
 {
-    //ответы имеются разных типов
-    // RBS, S, НРЗ
-    //типичная информация ответа RBS
-    POI::ExtendedInfo info;
-    info.rbs_modeOther.Code = 01234;  //код ответа
-    info.rbs_modeOther.nCode = 00000; // биты недостоверности кода: 0 - достоверно, 1 - недостоверно
-    info.rbs_modeOther.SPI = true;    // Специальная индикация положения (SPI)
-    info.rbs_modeOther.nSPI = false;  // бит недостоверности
-    info.rbs_modeOther.AW = false;    // авария
-    info.rbs_modeOther.PBL = false;   // 0 - над ПБЛ, 1 - под ПБЛ;
-                                      // под ПБЛ значит ответ не должен участвовать
-                                      // в формировании информации по самолёту
-
-
-    // самолёт, или как мы называем координатная точка
-    // тоже бывает разных типов
-    // RBS/S
     impl::CoordinatePoint cp;
-    cp.modeS; //тут информация по режиму S; возможно есть лишнее. можно на первых порах исключить вектора
+    DB::PostgresCPRepository PRep("postgresql://localhost:5432/default_database?user=username&password=password", false);
+//    PRep.addCoordinatePoint(cp);
+    PRep.getByIdCoordinatePoint("5");
+    auto responseCP = PRep.getByIdCoordinatePoint("5");
+    std::cout << "idcp: " << responseCP.idCoordinatePoint << std::endl;
+    std::cout << "range: " << responseCP.range << std::endl;
+    std::cout << "amplitude: " << responseCP.amplitude << std::endl;
+    std::cout << "mode: " << responseCP.mode << std::endl;
+    std::cout << "heightIsGarbled: " << responseCP.heightIsGarbled << std::endl;
+
+//    //ответы имеются разных типов
+//    // RBS, S, НРЗ
+//    //типичная информация ответа RBS
+////    POI::ExtendedInfo info;
+////    info.modeS.data;
+////    info.modeS.BDS1;
+////    info.modeS.BDS2;
+////    info.modeS.PC_EC;
+////    info.modeS.sicAllCall;
+////    info.rbs_modeOther.AW;
+////    info.rbs_modeOther.Code;
+////    info.rbs_modeOther.PBL;
+////    info.rbs_modeOther.SPI;
+////    info.rbs_modeOther.nCode;
+////    info.rbs_modeOther.nSPI;
+////    info._reserved;
+////    info._ser_value;
+////    info.parol_modeAlert.value;
+////    info.peleng_ASHP.value;
+////    info.peleng_HIP.skw;
+////    info.rbs_mode4.PBL;
+////    info.rbs_mode4.Gr;
+////    info.rbs_mode4.KodM4;
+////    info.rbs_mode4.POKM4;
+//
+//
+//
+//    // самолёт, или как мы называем координатная точка
+//    // тоже бывает разных типов
+//    // RBS/S
+//    impl::CoordinatePoint cp;
+////    cp.modeS.altitude;
+////    cp.modeS.AA;
+////    cp.modeS.acasParameters;
+////    cp.modeS.acasReportAdvisory;
+////    cp.modeS.acasReportAdvisoryIsPresent;
+////    cp.modeS.acasStatus;
+////    cp.modeS.aircraftIdentification;
+////    cp.modeS.aircraftIdentificationCapability;
+////    cp.modeS.altitudeReportingCapability;
+////    cp.modeS.bds10Received;
+////    cp.modeS.flightStatus;
+////    cp.modeS.identity;
+////    cp.modeS.mbdata;
+////    cp.modeS.msscCapability;
+////    cp.modeS.nextRequests;
+////    cp.modeS.outdatedBds;
+////    cp.modeS.recentlyUpdatedBds;
+////    cp.modeS.si;
+////    cp.modeS.supportedBds;
+////    cp.modeS.transponderLevel;
+////    cp.modeS.unrequestedBds;
+////    cp.modeS.usefullDataBds;
+//
+////    +idCoordinatePoint
+////    +timeCoordinatePoint
+//    cp.idCoordinatePoint;
+//    cp.timestamp;
+//    cp.range;
+//    cp.azimuth;
+//    cp.amplitude;
+//    cp.mode;
+//    cp.missionCode;
+//    cp.unitCode;
+//    cp.bortNumber;
+//    cp.altitude;
+//    cp.spi;
+//    cp.xImpulse;
+//    cp.militaryAlarm;
+//    cp.isRollCall;
+//    cp.isControlResponder;
+//    cp.missionCodeIsGarbled;
+//    cp.unitCodeIsGarbled;
+//    cp.bortNumberIsGarbled;
+//    cp.heightIsGarbled;
+//
+////    cp.mark;
+////    cp.rejectedByTagc;
+////    cp.unitCode;
+//
+//
+////    +idCoordinatePoint;
+//    cp.pack[0]->raw.timestamp; //!!!
+//    cp.pack[0]->distance; //processedDistance
+//    cp.pack[0]->raw.radio.azimuth;
+//    cp.pack[0]->phase; //processedPhase
+//    cp.pack[0]->raw.radio.amplitude.sum; //sum_ampl
+//    cp.pack[0]->raw.radio.amplitude.dif; //dif_ampl
+//    cp.pack[0]->raw.radio.amplitude.pbl; //pbl_ampl
+//    cp.pack[0]->raw.info.mode;
+//
+//    //RBS
+//    cp.pack[0]->raw.info.exInfo.rbs_modeOther.Code;
+//    cp.pack[0]->raw.info.exInfo.rbs_modeOther.SPI;
+//    cp.pack[0]->raw.info.exInfo.rbs_modeOther.nCode;
+//    cp.pack[0]->raw.info.exInfo.rbs_modeOther.nSPI;
+//    cp.pack[0]->raw.info.exInfo.rbs_modeOther.PBL; //isUnderPbl
+//    cp.pack[0]->raw.info.exInfo.rbs_modeOther.AW;
+//
+//    //modeS
+//    cp.pack[0]->raw.info.exInfo.modeS.data;
+//    cp.pack[0]->raw.info.exInfo.modeS.sicAllCall;
+//    cp.pack[0]->raw.info.exInfo.modeS.PC_EC;
+//    cp.pack[0]->raw.info.exInfo.modeS.BDS2;
+//    cp.pack[0]->raw.info.exInfo.modeS.BDS1;
+//    cp.pack[0]->raw.info.exInfo.modeS.PC_type;
+//
+//    cp.pack[0]->monopulseAzimuth;
+//    cp.pack[0]->monopulseCorrection;
+//    cp.pack[0]->raw.radio.phase;
+//    cp.pack[0]->raw.radio.distance;
+//    cp.pack[0]->azimuthFromDistance;
+//
+//
+//
+//
+////    cp.pack[0]->raw.info.ASP;
+////    cp.pack[0]->raw.info.Dos;
+////    cp.pack[0]->raw.info.HIP;
+////    cp.pack[0]->raw.info.dDS;
+////    cp.pack[0]->raw.info.values;
+//
+//
+//
+//
+
+
+
+
+
+
+
+    //тут информация по режиму S; возможно есть лишнее. можно на первых порах исключить вектора
     //и НРЗ также синонимы Пароль/ЕС ГРЛО
-    esgrlo::CoordinatePoint nrzCp;
+//    esgrlo::CoordinatePoint nrzCp;
 }
